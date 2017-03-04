65c322abfd49b090e058b4ce53873d98aeccf493
rkellerman <ryan.kellerman@rutgers.edu>
2017-03-04 17:42:24 -0500

    App now jumps to new page after login screen, able to pass player object to the new activity...

--- app/src/main/java/com/example/stockers/WelcomeActivity.java
+++ app/src/main/java/com/example/stockers/WelcomeActivity.java
@@ -1,5 +1,6 @@
 package com.example.stockers;
 
+import android.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,5 +11,12 @@ public class WelcomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
+        Player player = (Player)getIntent().getSerializableExtra("Player");
+
+        AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+        alertDialog.setTitle("Info");
+
+        alertDialog.setMessage("Player name: " + player.name + "\nPlayerID: " + player.playerID + "\nPlayer money: $" + player.value);
+        alertDialog.show();
     }
 }
