1e635a1ede416b4d7491574630f988aa55ca679d
rkellerman <ryan.kellerman@rutgers.edu>
2017-03-19 20:15:52 -0400

    Worked on Trade page

--- app/src/main/java/com/example/stockers/tradeActivity.java
+++ app/src/main/java/com/example/stockers/tradeActivity.java
@@ -5,17 +5,65 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class tradeActivity extends Fragment {
 
+    ArrayAdapter<String> adapter;
+    ListView listView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.trade_layout, container, false);
+        listView = (ListView)getView().findViewById(R.id.listView);
+
+        ArrayList<String> arrayList = new ArrayList<String>();
+        arrayList.addAll(Arrays.asList(getResources().getStringArray(R.array.array_stocks)));
+
+        adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, arrayList);
+        listView.setAdapter(adapter);
+
+
 
         return rootView;
+    }
+
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+
+        inflater.inflate(R.menu.menu_search, menu);
+        MenuItem item = menu.findItem(R.id.menuSearch);
+        SearchView searchView = (SearchView)item.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener(){
+
+            @Override
+            public boolean onQueryTextSubmit(String query){
+                // make the whole shit go away
+                listView.setVisibility(View.GONE);
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText){
+                adapter.getFilter().filter(newText);
+
+                return false;
+            }
+        });
+
+        super.onCreateOptionsMenu(menu, inflater);
     }
 
     @Override
